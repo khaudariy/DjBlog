ae728d95deb0b42f1fff38d4a65ddda4f27858fa
Hossam <hossamkhaudariy0@gmail.com>
2024-03-16 17:15:03 +0200

    add admin customization

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','draft']
+    list_filter = ['draft']
+    search_fields = ['title']
+admin.site.register(Post,ProductAdmin)
