54dde5f5c1d726da9fbafaf7901ffdee2369731b
Hossam <hossamkhaudariy0@gmail.com>
2024-03-22 14:03:15 +0200

    add  media and static files

--- posts/admin.py
+++ posts/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
+from django.utils import timezone 
 from .models import Post
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title','draft']
-    list_filter = ['draft']
-    search_fields = ['title']
+    list_display = ['title','draft','publish_date','get_tags']
+    list_filter = ['draft','tags']
+    search_fields = ['title',]
+    def get_tags(self, post):
+        tags = []
+        for tag in post.tags.all():
+            tags.append(str(tag))
+        return ', '.join(tags)
 admin.site.register(Post,ProductAdmin)
--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.utils import timezone 
+from taggit.managers import TaggableManager
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=20000)
     draft = models.BooleanField(default=True)
+    publish_date = models.DateTimeField(timezone.now)
+    tags = TaggableManager()
+
+
+
     def __str__(self):
         return self.title
