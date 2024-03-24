e5ac2630b1dc1280475367458aca292f8eb331f6
Hossam <hossamkhaudariy0@gmail.com>
2024-03-25 00:58:33 +0200

    make first view for the post

--- posts/views.py
+++ posts/views.py
@@ -9,4 +9,10 @@ def post_list(request):
     }
     return render(request,'posts/post_list.html',context)
 
+def post_detail(request,post_id):
+    data = Post.objects.get(id=post_id)
+    context = {
+        'post' : data
+    }
+    return render (request , 'posts/post_detail.html',context)
 
