4117ae9e4c4c3429bf514c20391d184392e855c1
Hossam <hossamkhaudariy0@gmail.com>
2024-03-24 23:48:56 +0200

    make first view for the post

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+def post_list(request):
+    data = Post.objects.all() 
+    context = {
+        'mahmoud' : data
+    }
+    return render(request,'posts/post_list.html',context)
+
+
