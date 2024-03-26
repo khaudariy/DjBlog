44333e53bd38600617d2d71b158a2c9621ae19b6
Hossam <hossamkhaudariy0@gmail.com>
2024-03-26 04:30:48 +0200

    make summernote for the post content

--- posts/admin.py
+++ posts/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.utils import timezone 
 from .models import Post ,Category, Comment
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(SummernoteModelAdmin):
     list_display = ['title','draft','publish_date','get_tags','author','category']
     list_filter = ['draft','tags','category']
     search_fields = ['title',]
+    summernote_fields = ('content')
     def get_tags(self, post):
         tags = []
         for tag in post.tags.all():
--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
 from posts.views import post_list , post_detail
@@ -23,7 +23,8 @@ from posts.views import post_list , post_detail
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/',post_list),
-    path('posts/<int:post_id>',post_detail)
+    path('posts/<int:post_id>',post_detail),
+    path('summernote/', include('django_summernote.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
