58b6634b3f46378730405325f5c34f8b8ee49cb3
Hossam <hossamkhaudariy0@gmail.com>
2024-03-28 23:33:22 +0200

    make form to edit post

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Post
 from .forms import PostForm
 from django.views.generic import ListView , DetailView
@@ -9,12 +9,30 @@ def create_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST,request._files)
         if form.is_valid():
+            myform = form.save(commit=False)
+            myform.author = request.user
+            myform.save()
             form.save()
+            return redirect('/posts/')
     else:    
         form = PostForm()
 
     return render(request , 'posts/new.html' , {'form':form})
 
+def edit_post(request,pk2 ):
+    post = Post.objects.get(id=pk2)
+    if request.method == 'POST':
+        form = PostForm(request.POST,request._files,instance=post)
+        if form.is_valid():
+            myform = form.save(commit=False)
+            myform.author = request.user
+            myform.save()
+            form.save()
+            return redirect('/posts/')
+    else:    
+        form = PostForm(instance=post)
+
+    return render(request , 'posts/edit.html' , {'form':form})
 class PostList(ListView):
     model = Post
 
