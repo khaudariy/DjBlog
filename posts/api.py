79122b382a0fc09ec4c78bdcc5568154f711071c
Hossam <hossamkhaudariy0@gmail.com>
2024-04-04 17:30:30 +0200

    make API for the post detail

--- posts/api.py
+++ posts/api.py
@@ -8,3 +8,8 @@ def post_list_api(request):
     posts = Post.objects.all()
     data = PostSerializer(posts,many=True).data
     return Response({'data':data})
+@api_view(['GET'])
+def post_detail_api(request,id):
+    post = Post.objects.get(id=id)
+    data = PostSerializer(post).data
+    return Response({'data':data})
