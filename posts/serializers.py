79c9a7de05ad5d02ddc58cc9dc0ce67d94adb917
Hossam <hossamkhaudariy0@gmail.com>
2024-04-05 04:37:55 +0200

    make API for the post list / detail and use the generics inhertance  and put filter and search with author name and content

--- posts/serializers.py
+++ posts/serializers.py
@@ -3,6 +3,8 @@ from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
+    category = serializers.StringRelatedField()
     class Meta:
        model = Post
        fields = '__all__'
